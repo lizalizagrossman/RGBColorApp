d249995983a7e2c3309a1383aa7994f713aea352
Liza Grossman <lisok.sam@gmail.com>
2023-04-25 19:02:28 +0300

    Initial Commit

---
+++ RGBColorApp/RGBColorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBColorAppApp.swift
+//  RGBColorApp
+//
+//  Created by Elizabeth on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct RGBColorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
